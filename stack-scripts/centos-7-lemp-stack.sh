b309208acd3c8db8e52645a368aa9472355b2bcf
cmdobueno <d.good261@gmail.com>
2018-08-06 11:10:43 -0500

    Fixed SELinux Issues
    Rewrite for the SELinux to allow it to be disabled.

--- stack-scripts/centos-7-lemp-stack.sh
+++ stack-scripts/centos-7-lemp-stack.sh
@@ -46,7 +46,21 @@ echo "#### Install Start ####"
 # Disable SELinux (this will require a server reboot after install is complete)
 if [[ $SELINUX = 'no' ]]; then
     setenforce 0
-    sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/" /etc/sysconfig/selinux
+    truncate -s 0 /etc/sysconfig/selinux
+cat <<EOT >> /etc/sysconfig/selinux
+# This file controls the state of SELinux on the system.
+# SELINUX= can take one of these three values:
+#     enforcing - SELinux security policy is enforced.
+#     permissive - SELinux prints warnings instead of enforcing.
+#     disabled - No SELinux policy is loaded.
+SELINUX=disabled
+# SELINUXTYPE= can take one of three two values:
+#     targeted - Targeted processes are protected,
+#     minimum - Modification of targeted policy. Only selected processes are protected.
+#     mls - Multi Level Security protection.
+SELINUXTYPE=targeted
+
+EOT
 fi
 
 # Update yum
