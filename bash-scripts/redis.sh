c2da52ab75aff5d9cb28d9e892e216d3d1fb3636
Randall Wilk <wilk.randall@gmail.com>
2018-08-12 15:54:01 -0500

    Add sudo to allow sudo users to install

--- bash-scripts/redis.sh
+++ bash-scripts/redis.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Update server
-yum -y update
+sudo yum -y update
 
 # Install Redis
-yum -y install redis
+sudo yum -y install redis
 
 # Start redis
-systemctl start redis
-systemctl enable redis
+sudo systemctl start redis
+sudo systemctl enable redis
 
 # Configure redis
-sed -i -e "s/appendonly no/appendonly yes/" /etc/redis.conf
+sudo sed -i -e "s/appendonly no/appendonly yes/" /etc/redis.conf
 
 # Restart redis
-systemctl restart redis
+sudo systemctl restart redis
 
 # Tuning
-sysctl vm.overcommit_memory=1
-echo vm.overcommit_memory=1 >> /etc/sysctl.conf
+sudo sysctl vm.overcommit_memory=1
+sudo echo vm.overcommit_memory=1 >> /etc/sysctl.conf
