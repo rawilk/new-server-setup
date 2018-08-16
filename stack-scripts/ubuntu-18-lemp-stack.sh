0c48ae75fcb97a1e3544117859f4df5835ae0294
Randall Wilk <wilk.randall@gmail.com>
2018-08-15 21:51:33 -0500

    Update stack script

--- stack-scripts/ubuntu-18-lemp-stack.sh
+++ stack-scripts/ubuntu-18-lemp-stack.sh
@@ -35,6 +35,9 @@ echo "#### Install Start ####"
 # Update the system
 apt update && apt upgrade -y
 
+# Install expect (will be removed at the end)
+apt install -y expect
+
 # Set the hostname
 hostnamectl set-hostname $FQDN --static
 
@@ -94,7 +97,7 @@ systemctl restart sshd
 # TODO: cron-apt
 
 # Setup fail2ban
-apt-get install -y fail2ban
+apt install -y fail2ban
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.loal
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sed -i -e "s/backend = .*/backend = systemd/" /etc/fail2ban/jail.local
@@ -105,7 +108,7 @@ systemctl start fail2ban
 stty erase ^H
 
 # Setup FTP
-apt-get install -y vsftpd
+apt install -y vsftpd
 
 # Create FTP user
 adduser --disabled-password --gecos "" $FTP_USER_NAME
@@ -140,14 +143,172 @@ apt install -y nginx
 ufw allow 'Nginx HTTP'
 
 # Install MariaDB
+export DEBIAN_FRONTEND=noninteractive
+
+debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password password $ROOT_DB_PASSWORD'
+debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password_again password $ROOT_DB_PASSWORD'
+
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64] http://mirror.zol.co.zw/mariadb/repo/10.3/ubuntu bionic main'
 apt install -y mariadb-server mariadb-client
 
-# TODO: secure install
+# Run through the secure installation
+SECURE_MYSQL=$(expect -c "
 
-# Install PHP
-apt-get install -y php php-fpm php-common php-bcmath php-gd php-mbstring php-xmlrpc php-mysql php-imagick php-xml
+set timeout 3
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none):\"
+send \"$ROOT_DB_PASSWORD\r\"
+
+expect \"Change the root password? [Y/n]\"
+send \"n\r\"
+
+expect \"Remove anonymous users?\"
+send \"y\r\"
+
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+# Bind IP to mysql
+cat <<EOT >> /etc/my.cnf
+[mysqld]
+bind-address=$IPADDR
+EOT
+
+# Restart mariadb
+systemctl restart mariadb
+
+# Allow mariadb through firewall
+ufw allow mysql
+
+# Setup remote db access
+mysql -u root -p$ROOT_DB_PASSWORD -e "GRANT ALL ON *.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' WITH GRANT OPTION;FLUSH PRIVILEGES;"
+
+# Install PHP (The order here matters; install php-fpm before php to avoid installing apache!)
+apt install -y php-fpm php-common php-bcmath php-gd php-mbstring php-xmlrpc php-mysql php-imagick php-xml php-zip
+
+# Configure PHP
+sed -i -e "s/upload_max_filesize = 2M/upload_max_filesize = 64M/" /etc/php/7.2/fpm/php.ini
+sed -i -e "s/memory_limit = 128M/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
+sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.2/fpm/php.ini
+
+# Configure Nginx
+truncate -s 0 /etc/nginx/nginx.conf
+cat <<EOT >> /etc/nginx/nginx.conf
+user $FTP_USER_NAME;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+
+    gzip on;
+    gzip_comp_level 3;
+    gzip_min_length 1000;
+    gzip_proxied expired no-cache no-store private auth;
+    gzip_types text/plain application/x-javascript text/xml text/css application/xml;
+
+    client_max_body_size 64M;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+    include /etc/nginx/conf.d/*.conf;
+}
+EOT
+
+# Configure site server block
+touch /etc/nginx/conf.d/$HOSTNAME.conf
+cat <<EOT >> /etc/nginx/conf.d/$HOSTNAME.conf
+server {
+    listen     80;
+
+    server_name  $IPADDR www.$FQDN $FQDN;
+    root         /home/$FTP_USER_NAME/public_html/public;
+
+    access_log  /home/$FTP_USER_NAME/logs/access.log;
+    error_log  /home/$FTP_USER_NAME/logs/error.log;
+
+    client_max_body_size 1024M;
+    index index.php index.html;
+
+    location /favicon.ico {
+        access_log off;
+        expires max;
+    }
+
+    location /robots.txt {
+        access_log off;
+        expires max;
+    }
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~* ^.+.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)$ {
+        access_log        off;
+        expires           max;
+    }
+
+    location ~ \.php\$ {
+        try_files \$uri =404;
+        fastcgi_pass unix:/run/php-fpm/www.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include /etc/nginx/fastcgi_params;
+        client_max_body_size 1024M;
+    }
+
+    error_page 404 /404.html;
+        location = /40x.html {
+    }
+
+    error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+    }
+}
+EOT
+
+# Init site folder
+mkdir -p /home/$FTP_USER_NAME/logs
+mkdir -p /home/$FTP_USER_NAME/public_html/public
+
+# Create a test page
+touch /home/$FTP_USER_NAME/public_html/public/index.php
+echo "<?php phpinfo(); ?>" >> /home/$FTP_USER_NAME/public_html/public/index.php
+
+# Give FTP user ownership of the folders
+chown -R $FTP_USER_NAME:$FTP_USER_NAME /home/$FTP_USER_NAME
 
 # Install composer
 cd /tmp
@@ -155,7 +316,15 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install Node and NPM
-apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install -y nodejs
+npm install npm@latest -g
 
 # Enable the firewall
 ufw enable
+
+# Clean up
+apt remove -y expect
+apt purge -y expect
+
+echo "#### Install Complete! ####"
