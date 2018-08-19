96f5e5397385168511160e615cc80cf260f60d47
Randall Wilk <wilk.randall@gmail.com>
2018-08-19 16:17:42 -0500

    Includes bash script

---
+++ stack-scripts/functions/common/includes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Author: Randall Wilk <randall@randallwilk.com>
+
+##############################################
+# Include the given github shell script.
+# Globals:
+#    None
+# Arguments:
+#   Script Path, Output File (Optional)
+# Returns:
+#   None
+#############################################
+function include_github_script() {
+    local BASE=https://raw.githubusercontent.com/rawilk/new-server-setup/master/
+    local OUTPUT=/root/includes.sh
+    
+    if [[ $2 != '' ]]; then
+        local OUTPUT="$2"
+    fi
+
+    curl ${BASE}$1 >> ${OUTPUT}
+}
