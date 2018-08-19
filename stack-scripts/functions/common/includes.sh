a8c0c311bf1552d25d60a6835e9e6392e12c7c99
Randall Wilk <wilk.randall@gmail.com>
2018-08-19 16:47:42 -0500

    Update default output file and add documentation to the function

--- stack-scripts/functions/common/includes.sh
+++ stack-scripts/functions/common/includes.sh
@@ -12,14 +12,16 @@
 #############################################
 function include_github_script() {
     local BASE=https://raw.githubusercontent.com/rawilk/new-server-setup/master/
-    local OUTPUT=/root/includes.sh
-    
+    local OUTPUT=/root/stackfunctions.sh
+
+    # Update the output file if one is provided
     if [[ $2 != '' ]]; then
         local OUTPUT="$2"
     fi
 
     local URI="$BASE$1"
 
+    # Append extra lines first if the file already has content
     if [[ -s $OUTPUT ]]; then
         echo "" >> $OUTPUT
         echo "" >> $OUTPUT
