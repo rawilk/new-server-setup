2a66e9f302963229ec6039ebed9beebdb7206e64
Randall Wilk <wilk.randall@gmail.com>
2018-08-20 12:54:49 -0500

    Formatting

--- stack-scripts/functions/debian-based/database.sh
+++ stack-scripts/functions/debian-based/database.sh
@@ -43,20 +43,28 @@ function install_mariadb() {
 function configure_mariadb() {
     # Run through the secure installation
     SECURE_MYSQL=$(expect -c "
+
     set timeout 3
     spawn mysql_secure_installation
+
     expect \"Enter current password for root (enter for none):\"
     send \"$ROOT_DB_PASSWORD\r\"
+
     expect \"Change the root password?\"
     send \"n\r\"
+
     expect \"Remove anonymous users?\"
     send \"y\r\"
+
     expect \"Disallow root login remotely?\"
     send \"y\r\"
+
     expect \"Remove test database and access to it?\"
     send \"y\r\"
+
     expect \"Reload privilege tables now?\"
     send \"y\r\"
+
     expect eof
     ")
 
